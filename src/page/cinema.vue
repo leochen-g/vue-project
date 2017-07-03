ce9fdf9925a27bb9d75e51119e744e4dc8298f6d
kiven_worker <478914121@qq.com>
2017-07-03 15:48:35 +0800

    完善院线列表样式

--- src/page/cinema.vue
+++ src/page/cinema.vue
@@ -1,20 +1,42 @@
 <template>
   <!--影院-->
-      <div id="cinema">
-        <mt-header fixed v-bind:title="title" >
-          <router-link to="/" slot="left">
-            <mt-button icon="back">返回</mt-button>
-          </router-link>
-          <mt-button icon="more" slot="right"></mt-button>
-        </mt-header>
-        <div class="cinema_content">
-          <div class="cinema_content_list" v-for="(item,index) in list" :key="index">
-              <div class="cinema_content_list_item">
-                <div class="item_cinema_name">{{item.cinema_name}}</div>
-              </div>
-          </div>
-        </div>
-      </div>
+  <div id="cinema">
+    <mt-header fixed v-bind:title="title">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <div class="cinema_content">
+      <ul class=" cinema-list" >
+        <li v-for="item in list">
+          <a data-login-required="true" class="cinema-item" href="">
+            <div class="sched-info">
+              <aside class="left-info">
+                <h3 class="cinema-name">{{item.cinema_name}}</h3>
+                <p class="cinema-address">{{item.area_name}} {{item.addr}}</p>
+                <div class="cinema-label">
+                  <p class="cinema-label-tags" >
+                    <span class="Label Label--secondary"v-for="tag in item.special">{{tag.text}}</span>
+                  </p>
+                  <p class="cinema-label-hui">
+                    <span class="Label special-hui">惠</span>
+                    <span>{{item.discount_des_all}}</span>
+                  </p>
+                </div>
+              </aside>
+              <aside class="right-info">
+                <span class="dis-sale">
+                  <span class="price">{{item.mem_price}}</span>
+                  <span class="grey">起</span>
+                </span>
+              </aside>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
   export default{
