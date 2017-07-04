7d33a0ccba662d9750147492ad8c4204330c0803
kiven_worker <478914121@qq.com>
2017-07-04 17:43:47 +0800

    解决刷新，tab菜单重置

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
     <!--content-->
     <router-view></router-view>
     <mt-tabbar v-model="selected" fixed >
-      <mt-tab-item id="movie" isRouter>
+      <mt-tab-item id="movie" >
         <img slot="icon" src="./assets/movie.png">
         影片
       </mt-tab-item>
-      <mt-tab-item id="cinema" isRouter>
+      <mt-tab-item id="cinema" >
         <img slot="icon" src="./assets/cinema.png">
         影院
       </mt-tab-item>
-      <mt-tab-item id="home" isRouter>
+      <mt-tab-item id="home" >
         <img slot="icon" src="./assets/home.png">
         我的
       </mt-tab-item>
@@ -28,16 +28,29 @@
         selected: 'movie'
       }
     },
+    created: function () {
+      if (localStorage.selected) {
+        this.selected = localStorage.selected
+      }
+    },
     watch: {
       selected: function (val, oldval) {
         if (val === 'movie') {
+          localStorage.selected = val
           this.$router.push('/')
         } else if (val === 'cinema') {
+          localStorage.selected = val
           this.$router.push('/cinema')
-        } else {
+        } else if (val === 'home') {
+          localStorage.selected = val
           this.$router.push('/home')
+        } else {
+          localStorage.selected = 'movie'
+          this.$router.push('/')
         }
       }
+    },
+    methods: {
     }
   }
 </script>
